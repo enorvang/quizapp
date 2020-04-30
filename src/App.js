6cdc0d127ce57eabf5d74eed83b6f670b94eaccb
enorvang <espen.norvang@gmail.com>
2020-04-30 17:36:33 +0200

    installed axios and local server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import questionServices from './services/questionServices';
 
-function App() {
+
+
+const App = () => {
+  const [questions, setQuestions] = useState([]);
+
+
+
   return (
     <div>
-      <p></p>
+      <h1>QuizApp</h1>
     </div>
-  );
+  )
 }
 
 export default App;
